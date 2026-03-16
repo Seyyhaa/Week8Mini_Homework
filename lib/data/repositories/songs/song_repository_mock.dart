aff7d56659a88869087693c8727967e5e55cec98
Seyha Kruy <seyhakruy168168@gmail.com>
2026-03-16 08:15:59 +0700

    FIX AGAIN

--- lib/data/repositories/songs/song_repository_mock.dart
+++ lib/data/repositories/songs/song_repository_mock.dart
@@ -39,6 +39,7 @@ class SongRepositoryMock implements SongRepository {
 
   @override
   Future<List<Song>> fetchSongs() async {
+    await Future.delayed(Duration(seconds: 3));
     return _songs;
   }
 
